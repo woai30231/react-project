e236743ca9d1fc9ed1c7a2d4fc65e2bf88892313
sander <554830231@qq.com>
2025-06-19 11:16:52 +0800

    init

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>联系我们</h1>
+      <p>这里是联系我们页面的内容。</p>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Contact;
